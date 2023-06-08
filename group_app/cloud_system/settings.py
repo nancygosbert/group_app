fa301c8f891d6aa4af109b838118ac2c79d201e8
nancygosbert <135701826+nancygosbert@users.noreply.github.com>
2023-06-08 19:54:19 +0100

    Update settings.py

--- group_app/cloud_system/settings.py
+++ group_app/cloud_system/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os  
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-$$2f8%%6&styny#y)63e9!w@4$7(a3%u%n-r%h@4=ruv1^hp!k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
